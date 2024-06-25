d0c995854c57b531dce9381b6f0513a5bc9030e8
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-06-25 18:07:32 +0900

    websocket with dependencies

--- src/websocket_fastapi/main.py
+++ src/websocket_fastapi/main.py
@@ -1,10 +1,19 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import (
+    Cookie,
+    Depends,
+    FastAPI,
+    Query,
+    WebSocket,
+    WebSocketException,
+    status,
+)
 from fastapi.responses import HTMLResponse
+
 
 app = FastAPI()
 
 html = ""
-with open("./sample.html", "r", encoding="utf-8") as file:
+with open("./sample2.html", "r", encoding="utf-8") as file:
     html = file.read()
 
 
@@ -13,9 +22,29 @@ async def get():
     return HTMLResponse(html)
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+async def get_cookie_or_token(
+    websocket: WebSocket,
+    session: str | None = Cookie(default=None),
+    token: str | None = Query(default=None),
+):
+    if session is None and token is None:
+        raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
+    return session or token
+
+
+@app.websocket("/items/{item_id}/ws")
+async def websocket_endpoint(
+    websocket: WebSocket,
+    item_id: str,
+    q: int | None = None,
+    cookie_or_token: str | None = Depends(get_cookie_or_token),
+):
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        await websocket.send_text(
+            f"Session cookie or query token value is: {cookie_or_token}"
+        )
+        if q is not None:
+            await websocket.send_text(f"Query parameter q is : {q}")
+        await websocket.send_text(f"Message text was: {data}, for item ID: {item_id}")
